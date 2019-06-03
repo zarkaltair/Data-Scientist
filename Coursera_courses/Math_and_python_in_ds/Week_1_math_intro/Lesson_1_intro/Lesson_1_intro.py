d5c67e17abf292a0108124163229e30aec5f46a0
Zark_Altair <zarkaltair@gmail.com>
2019-06-03 20:41:54 +0500

    learn base python

---
+++ Coursera_courses/Math_and_python_in_ds/Week_1_math_intro/Lesson_1_intro/Lesson_1_intro.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import pandas as pd
+import numpy as np
+import scipy as sp
+import matplotlib as plt
+
+
+# In[5]:
+
+
+np.__version__
+
+
+# In[7]:
+
+
+pd.__version__
+
+
+# In[8]:
+
+
+sp.__version__
+
+
+# In[9]:
+
+
+plt.__version__
+
+
+# # “Машинное обучение и анализ данных”
+
+# ## “Математика и Python”
+
+# "Задание 1"
