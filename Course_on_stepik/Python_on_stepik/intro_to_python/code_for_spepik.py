abf775319676e8c7c0c045d14712527549aea733
zarkaltair <zarkaltair@gmail.com>
2019-10-31 21:46:58 +0500

    rename folder

---
+++ Course_on_stepik/Python_on_stepik/intro_to_python/code_for_spepik.py
@@ -0,0 +1,238 @@
+from pprint import pprint
+
+
+# def modify_list(l):
+#     for i in l:
+#         if i % 2 != 0:
+#             l.remove(i)
+#         else:
+#             # l.pop(i)
+#             l.append(i)
+#             # pass
+
+# l = [1,2,3,4,5,6,7,8]
+# modify_list(l)
+# print(l)
+
+
+# def modify_list(l):
+#     for i in range(len(l) - 1, -1, -1):
+#         if l[i] % 2 != 0:
+#             print(l[i])
+#             del l[i]
+#             print(l)
+#         else:
+#             print(l[i])
+#             l[i] = int(l[i] / 2) #int(i /2)
+#             print(l)
+
+# l = [1,2,3,4,5,6]
+# modify_list(l)
+# print(l)
+
+
+# def modify_list(l):
+#     l[:] = [i//2 for i in l if not i % 2]
+
+# l = [1,2,3,4,5,6]
+# modify_list(l)
+# print(l)
+
+
+# def modify_list(l):
+#     l[:] = [int(i / 2) for i in l if i % 2 == 0]
+
+# l = [1,2,3,4,5,6]
+# modify_list(l)
+# print(l)
+
+
+# def update_dictionary(d, key, value):
+#     if key in d:
+#         d[key].append(value)
+#     elif key not in d:
+#         key = key * 2
+#         if key in d:
+#             d[key].append(value)
+#         else:
+#             d[key] = [value]
+
+# d = {}
+# print(update_dictionary(d, 1, -1))  # None
+# print(d)                            # {2: [-1]}
+# update_dictionary(d, 2, -2)
+# print(d)                            # {2: [-1, -2]}
+# update_dictionary(d, 1, -3)
+# print(d)                            # {2: [-1, -2, -3]}
+
+
+# s = 'a aa abC aa ac abc bcd a'
+
+# def words_count(s):
+#     arr = {i: s.count(i) for i in s.lower().split(' ')}
+    
+#     return arr
+
+# print(words_count(s))
+
+
+# arr = {'x': 5}
+
+# if 'x' in arr:
+#     print('Yes')
+# else:
+#     print('No')
+
+# n = int(input())
+# z = 0
+# arr = {}
+# while z < n:
+#     z += 1
+#     x = int(input())
+#     if x not in arr:
+#         a = f(x)
+#         arr[x] = a
+#         print(a)
+#     else:
+#         print(arr[x])
+
+# a = [int(input()) for i in range(int(input()))]
+# b = {x: f(x) for x in set(a)}
+# for i in a:
+#     print(b[i])
+
+
+# import sys
+
+# for i in sys.argv:
+#     print(i)
+    # pass
+
+# print(sys.argv)
+# print(dir(sys.argv))
+# print(*sys.argv)
+
+
+# print(__import__("math").pi)
+
+# print(__import__('numpy').pi)
+
+
+# from itertools import groupby
+
+# with open('dataset_3363_2.txt', 'r') as file:
+# # with open('input.txt', 'r') as file:
+#     s = file.readline().strip()
+
+# arr = [''.join(i) for _, i in groupby(s, str.isalpha)]
+# arr_alpha = [i for i in arr if i.isalpha()]
+# arr_digit = [i for i in arr if i.isdigit()]
+
+# # print(len(arr_alpha))
+# # print(len(arr_digit))
+# ans = ''.join([arr_alpha[i] * int(arr_digit[i]) for i in range(len(arr_alpha))])
+# print(ans)
+# with open('answer_1.txt', 'w') as file:
+#     file.write(ans)
+
+
+# from collections import Counter
+
+# arr = []
+# with open('dataset_3363_3.txt', 'r') as file:
+#     for line in file:
+#         s = line.strip().split(' ')
+#         for i in s:
+#             arr.append(i.lower())
+
+# c = Counter(arr).most_common(1)
+# ans = ' '.join([str(i) for i in c[0]])
+# with open('answer_2.txt', 'w') as file:
+#     file.write(ans)
+
+
+# arr_mean = []
+# arr_math, arr_fis, arr_rus = [], [], []
+# with open('dataset_3363_4.txt', 'r') as file:
+#     for line in file:
+#         arr = [int(i) for i in line.strip().split(';') if i.isdigit()]
+#         arr_mean.append(str(sum(arr) / len(arr)))
+#         arr_math.append(arr[0])
+#         arr_fis.append(arr[1])
+#         arr_rus.append(arr[2])
+
+# with open('answer_3.txt', 'w') as file:
+#     file.write('\n'.join(arr_mean))
+#     file.write('\n' + str(sum(arr_math) / len(arr_math)) + ' ')
+#     file.write(str(sum(arr_fis) / len(arr_fis)) + ' ')
+#     file.write(str(sum(arr_rus) / len(arr_rus)))
+
+
+# import requests
+
+# with open('dataset_3378_2.txt', 'r') as file:
+#     s = file.read().strip()
+#     res = requests.get(s).text
+#     ans = res.splitlines()
+
+# with open('answer_4.txt', 'w') as file:
+#     file.write(str(len(ans)))
+
+
+# import requests
+
+# def get_file(url):
+#     return requests.get(url).text
+
+# with open('dataset_3378_3.txt', 'r') as file:
+#     s = file.read().strip()
+#     res = get_file(s)
+#     while 'We' not in res:
+#         url = 'https://stepic.org/media/attachments/course67/3.6.3/' + res
+#         res = get_file(url)
+#         print(res)
+        
+# with open('answer_5.txt', 'w') as file:
+# 	file.write(res)
+
+'''
+n, x = int(input()), 0
+arr = []
+while x != n:
+    x += 1
+    arr.append(input().split(' '))
+x = sum([int(i[1]) if i[0] == 'восток' else -int(i[1]) if i[0] == 'запад' else 0 for i in arr])
+y = sum([int(i[1]) if i[0] == 'север' else -int(i[1]) if i[0] == 'юг' else 0 for i in arr])
+print(x, y)
+
+
+dict = {'север': 0, 'юг': 0, 'запад': 0, 'восток': 0}
+
+for _ in range(int(input())):
+    key, value = input().split()
+    dict[key] += int(value)
+
+print(dict['восток'] - dict['запад'], dict['север'] - dict['юг'])
+
+
+with open('dataset_3380_5.txt', 'r') as file:
+    dic = {}
+    for line in file:
+        arr = [int(i) for i in line.strip().split('\t') if i.isdigit()]
+        dict_line = dict(zip(arr[:1], arr[1:]))
+        for key, value in dict_line.items():
+            if key in dic:
+                dic[key] += [value]
+            else:
+                dic[key] = [value]
+    d = sorted(dic)
+    for i in d:
+        print(i, sum(dic.get(i)) / len(dic.get(i)))
+    # pprint(d)
+'''
+
+
+n, x = int(input()), 0
+while x != n:
+    x += 1
+    arr = input().split(';')
---
+++ Course_on_stepik/Python_on_stepik/intro_to_python/intro.py
@@ -0,0 +1,38 @@
+# print(42 / (4 + 2 * -2))
+
+# print(2014 ** 14)
+
+# print(2014.0 ** 14)
+
+# print(7 / 3)
+
+# print(7 // 3)
+
+# print(round(2.99))
+
+# print(9 ** 19 - int(float(9 ** 19)))
+
+# x = 475
+# y = 1
+# z = 55
+
+# hours = int(x // 60)
+# minutes = int(x % 60)
+
+# if z + minutes < 60:
+#     print(y + hours)
+#     print(z + minutes)
+# else:
+#     print(y + hours + (z + minutes) // 60)
+#     print((z + minutes) % 60)
+
+# x = 5
+# y = 10
+# print(y > x * x or y >= 2 * x and x < y)
+
+# a = True
+# b = False
+# print(a and b or not a and not b)
+
+import math
+print(math.sqrt(9))
