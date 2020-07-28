398e16ef4a1af1b370c7cedafe7e4f11cf654333
Zark_Altair <zarkaltair@gmail.com>
2020-07-28 19:53:30 +0500

    learn bash scripts

---
+++ Course_on_stepik/Linux_on_stepik/script_4.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+var1="Hello, world!"
+var2="var1 is $var1"
+
+echo '$var2'
+echo "$var2"
+
---
+++ Course_on_stepik/Linux_on_stepik/script_5.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+var1=$1
+var2=$2
+
+echo "Script is $0, arguments are $var1 and $var2 (total $#)"
+
