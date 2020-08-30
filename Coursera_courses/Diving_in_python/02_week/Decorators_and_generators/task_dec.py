36ae8b6d31ccd6fafa99c1d17bf4c064937827e0
Zark_Altair <zarkaltair@gmail.com>
2020-08-30 09:44:27 +0500

    solution to decorators

--- Coursera_courses/Diving_in_python/02_week/Decorators_and_generators/task_dec.py
+++ Coursera_courses/Diving_in_python/02_week/Decorators_and_generators/task_dec.py
@@ -1,8 +1,12 @@
 import json
+import functools
 
 
 def to_json(func):
-    return json.dumps(func)
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+        return json.dumps(func(*args, **kwargs))
+    return wrapped
 
 @to_json
 def get_data():
