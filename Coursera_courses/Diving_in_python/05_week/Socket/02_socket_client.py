ba282fd61244102d78f773baf56a9103a72e53c0
Zark_Altair <zarkaltair@gmail.com>
2020-10-01 21:26:04 +0500

    little fix

--- Coursera_courses/Diving_in_python/05_week/Socket/02_socket_client.py
+++ Coursera_courses/Diving_in_python/05_week/Socket/02_socket_client.py
@@ -7,6 +7,6 @@ sock.sendall('ping'.encode('utf8'))
 sock.close()
 
 # short view
-sock = socket.create_connection(('127.0.0.1', 10001))
-sock.sendall('ping'.encode('utf8'))
-sock.close()
+# sock = socket.create_connection(('127.0.0.1', 10001))
+# sock.sendall('ping'.encode('utf8'))
+# sock.close()
--- Coursera_courses/Diving_in_python/05_week/Socket/05_socket_timeout_client.py
+++ Coursera_courses/Diving_in_python/05_week/Socket/05_socket_timeout_client.py
@@ -3,11 +3,12 @@ import socket
 
 
 with socket.create_connection(('127.0.0.1', 10001), 5) as sock:
+    print(sock)
     # set socket read timeout
-    sock.settimeout(2)
-    # try:
-    #     sock.sendall('ping'.encode('utf8'))
-    # except socket.timeout:
-    #     print('send data timeout')
-    # except socket.error as ex:
-    #     print('send data error:', ex)
+    # sock.settimeout(2)
+    try:
+        sock.sendall('ping'.encode('utf8'))
+    except socket.timeout:
+        print('send data timeout')
+    except socket.error as ex:
+        print('send data error:', ex)
