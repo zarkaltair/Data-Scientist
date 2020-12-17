41004e9e748cae9f21abf7426e850baae6f66564
Zark_Altair <zarkaltair@gmail.com>
2020-12-17 22:04:44 +0500

    test file

--- Coursera_courses/OOP_and_design_patterns/03_week/Assignments/Decorator/test_decorator.py
+++ Coursera_courses/OOP_and_design_patterns/03_week/Assignments/Decorator/test_decorator.py
@@ -39,7 +39,115 @@ class Hero:
 # =============================================================================
 # начало секции ВАШ КОД
 # =============================================================================
+class AbstractEffect(Hero, ABC):
+    def __init__(self, base):
+        self.base = base
 
+    @abstractmethod
+    def get_positive_effects(self):
+        return self.positive_effects
+
+    @abstractmethod
+    def get_negative_effects(self):
+        return self.negative_effects
+
+    @abstractmethod
+    def get_stats(self):
+        pass
+
+
+class AbstractPositive(AbstractEffect):
+    def get_negative_effects(self):
+        return self.base.get_negative_effects()
+
+
+class AbstractNegative(AbstractEffect):
+    def get_positive_effects(self):
+        return self.base.get_positive_effects()
+
+
+class Berserk(AbstractPositive):
+    def get_stats(self):
+        stats = self.base.get_stats()
+        stats['Strength'] += 7
+        stats['Endurance'] += 7
+        stats['Agility'] += 7
+        stats['Luck'] += 7
+        
+        stats['Perception'] -= 3
+        stats['Charisma'] -= 3
+        stats['Intelligence'] -= 3
+
+        stats['HP'] += 50
+        return stats
+
+    def get_positive_effects(self):
+        positive = self.base.get_positive_effects()
+        positive.append('Berserk')
+        return positive.copy()
+
+
+class Curse(AbstractNegative):
+    def get_stats(self):
+        stats = self.base.get_stats()
+        stats['Strength'] -= 2
+        stats['Perception'] -= 2
+        stats['Endurance'] -= 2
+        stats['Charisma'] -= 2
+        stats['Intelligence'] -= 2
+        stats['Agility'] -= 2
+        stats['Luck'] -= 2
+        return stats
+    
+    def get_negative_effects(self):
+        negative = self.base.get_negative_effects()
+        negative.append('Curse')
+        return negative.copy()
+
+
+class Blessing(AbstractPositive):
+    def get_stats(self):
+        stats = self.base.get_stats()
+
+        stats['Strength'] += 2
+        stats['Perception'] += 2
+        stats['Endurance'] += 2
+        stats['Charisma'] += 2
+        stats['Intelligence'] += 2
+        stats['Agility'] += 2
+        stats['Luck'] += 2
+
+    def get_positive_effects(self):
+        positive = self.base.get_positive_effects()
+        positive.append('Blessing')
+        return positive.copy()
+
+
+class Weakness(AbstractNegative):
+    def get_stats(self):
+        stats = self.base.get_stats()
+
+        stats['Strength'] -= 4
+        stats['Endurance'] -= 4
+        stats['Agility'] -= 4
+
+    
+    def get_negative_effects(self):
+        negative = self.base.get_negative_effects()
+        negative.append('Weakness')
+        return negative.copy()
+
+
+class EvilEye(AbstractNegative):
+    def get_stats(self):
+        stats = self.base.get_stats()
+
+        stats['Luck'] -= 10
+    
+    def get_negative_effects(self):
+        negative = self.base.get_negative_effects()
+        negative.append('EvilEye')
+        return negative.copy()
 # =============================================================================
 # конец секции ВАШ КОД
 # =============================================================================
