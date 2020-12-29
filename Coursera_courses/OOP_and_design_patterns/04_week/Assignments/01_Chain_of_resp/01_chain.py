98e82ca48df6c9ff1022c14fcf476f6a4a7193c6
Zark_Altair <zarkaltair@gmail.com>
2020-12-29 14:22:23 +0500

    add EventGet functional

--- Coursera_courses/OOP_and_design_patterns/04_week/Assignments/01_Chain_of_resp/01_chain.py
+++ Coursera_courses/OOP_and_design_patterns/04_week/Assignments/01_Chain_of_resp/01_chain.py
@@ -5,42 +5,88 @@ class SomeObject:
         self.string_field = ''
 
 
-class NullHandler:
+class EventGet:
     def __init__(self, kind):
         self.kind = kind
 
 
-class IntHandler():
+class EventSet():
     pass
 
 
-class FloatHandler():
-    pass
+class NullHandler:
+    def __init__(self, successor=None):
+        self.__successor = successor
+
+    def handle(self, obj, event):
+        if self.__successor is not None:
+            return self.__successor.handle(obj, event)
 
 
-class StrHandler():
-    pass
+class IntHandler(NullHandler):
+    def handle(self, obj, event):
+        if event.kind == int:
+            return obj.integer_field
+        else:
+            return super().handle(obj, event)
 
 
-chain = IntHandler(FloatHandler(StrHandler(NullHandler)))
+class FloatHandler(NullHandler):
+    def handle(self, obj, event):
+        if event.kind == float:
+            return obj.float_field
+        else:
+            return super().handle(obj, event)
+
+
+class StrHandler(NullHandler):
+    def handle(self, obj, event):
+        if event.kind == str:
+            return obj.string_field
+        else:
+            return super().handle(obj, event)
+
 
 obj = SomeObject()
 obj.integer_field = 42
 obj.float_field = 3.14
 obj.string_field = "some text"
+
 chain = IntHandler(FloatHandler(StrHandler(NullHandler)))
-chain.handle(obj, EventGet(int))
+
+print(chain.handle(obj, EventGet(int)))
 # 42
-chain.handle(obj, EventGet(float))
+print(chain.handle(obj, EventGet(float)))
 # 3.14
-chain.handle(obj, EventGet(str))
+print(chain.handle(obj, EventGet(str)))
 # 'some text'
+'''
 chain.handle(obj, EventSet(100))
 chain.handle(obj, EventGet(int))
 # 100
+
 chain.handle(obj, EventSet(0.5))
 chain.handle(obj, EventGet(float))
 # 0.5
+
 chain.handle(obj, EventSet('new text'))
 chain.handle(obj, EventGet(str))
 # 'new text'
+
+
+# предположим, что выше по коду у нас определены константы E_INT, E_FLOAT, E_STR
+# выделим из конструкции, переданной в print, словарь и поместим его в переменную types
+types = {int: E_INT, float: E_FLOAT, str: E_STR}
+# видим, что ключами в словаре служат типы данных int, float, str. Такое возможно, потому что это хэшируемые объекты. 
+# В этом вы можете убедиться сами:
+hash(int)  # вернет значение хэша
+hasattr(int, '__hash__')  # вернет True
+# таким образом у нас получается конструкция
+types[type(2)]
+# это обычное обращение к словарю для получения значения по ключу. В данном примере ключом выступает, 
+# возвращаемый из функции type результат
+type(2)  # вернет int, поскольку 2 - целое число и объект типа int
+# вся конструкция свелась к 
+types[int]
+# такой вызов вернет значение, лежащее в словаре по ключу int, а именно значение константы E_INT
+'''
